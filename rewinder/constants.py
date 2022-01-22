9134a805fa204d5da63b51da730bb6cbe1b3c256
Bharat Kalluri <bharatkalluri@protonmail.com>
2022-01-22 19:51:24 +0530

    piping logs to ~/.rewinder.logs

--- rewinder/constants.py
+++ rewinder/constants.py
@@ -11,4 +11,9 @@ RATE = 44100
 REWINDER_RECORDINGS_DIR = os.path.join(os.path.expanduser("~"), ".rewinder_recordings")
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(message)s",
+    level=logging.INFO,
+    filename=os.path.join(os.path.expanduser("~"), ".rewinder.logs"),
+    filemode="a",
+)
