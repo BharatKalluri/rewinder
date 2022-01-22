2e982c2ab04a49db35ff7f8763226e81a8dd6b70
Bharat Kalluri <bharatkalluri@protonmail.com>
2022-01-22 19:31:12 +0530

    improved file naming for out file

--- rewinder/backtrack.py
+++ rewinder/backtrack.py
@@ -1,10 +1,15 @@
 import glob
-import logging
 import os
 import time
 
 from .audio_utils import join_wav_files
 from .constants import BACKTRACK_RECORDINGS_DIR, logger
+
+
+def get_wav_file_name(start_time: int, end_time: int):
+    start_time_str = time.strftime('%H:%M', time.localtime(start_time))
+    end_time_str = time.strftime('%H:%M', time.localtime(start_time))
+    return f'{start_time_str}-{end_time_str}.wav'
 
 
 def rewind_audio(minutes: int):
@@ -15,7 +20,8 @@ def rewind_audio(minutes: int):
     start_times = [int(el.split("/")[-1].replace(".wav", "")) for el in list_of_files]
     sorted_start_times = list(sorted(start_times))
     wav_out_file = os.path.join(
-        os.path.expanduser("~"), f"{start_time}-{current_time}.wav"
+        os.path.expanduser("~"),
+        get_wav_file_name(start_time, int(current_time))
     )
     join_wav_files(
         [
